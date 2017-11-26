1db3ee9ca9f57af1207287ddd598d2e78169ad95
pabloevd950 <pabloevd950@gmail.com>
2017-11-26 17:29:22 -0600

    Added locking

--- CS4773Assignment4/src/model/MovieSingleton.java
+++ CS4773Assignment4/src/model/MovieSingleton.java
@@ -2,17 +2,23 @@ package model;
 
 public class MovieSingleton {
 
-	private static  MovieSingleton singleton = null;
+	//need an arbitrary object to manage the concurrency lock in getInstanceMultiThread2
+	private static final Object lock = new Object();
+
+	private static volatile MovieSingleton singleton = null;
 	public Movie movie = new Movie();
 	
 	private MovieSingleton() {
 	}
 
-	public static MovieSingleton getInstanceSingleThread() {
-		if(singleton == null) {
-			singleton = new MovieSingleton();
+	public synchronized static MovieSingleton getInstanceSingleThread() {
+		if (singleton == null) {
+			synchronized (lock) {
+				if (singleton == null) {
+					singleton = new MovieSingleton();
+				}
+			}
 		}
-
 		return singleton;
 	}
 	
